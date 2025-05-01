f154e024bc9ced21668f3ee1843a2f2fb2e7e6f5
Rifaiiii04 <muhamadrifay989@gmail.com>
2025-05-01 11:52:44 +0700

    ..

--- application/controllers/Profile.php
+++ application/controllers/Profile.php
@@ -4,6 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Profile extends CI_Controller {
     public function __construct() {
         parent::__construct();
+        $this->load->model('cart_model');
         if (!$this->session->userdata('logged_in')) {
             redirect('auth/login');
         }
