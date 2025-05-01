2cf400b09ae2e281f980be393ed228a610151641
Rifaiiii04 <muhamadrifay989@gmail.com>
2025-05-01 11:49:13 +0700

    refactor(Popular): update model loading and variable naming
    Standardize model loading by removing unnecessary model loads and updating variable names for consistency. The `Product_model` is renamed to `product_model` to follow naming conventions, and redundant model loads are removed to improve code maintainability.

--- application/controllers/Popular.php
+++ application/controllers/Popular.php
@@ -4,16 +4,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Popular extends CI_Controller {
     public function __construct() {
         parent::__construct();
-        $this->load->model('Product_model');
-        $this->load->model('Category_model');
-        $this->load->model('wishlist_model');
-        $this->load->library('session');
+        $this->load->model('cart_model');
+        $this->load->model('product_model');
+        $this->load->model('wishlist_model'); // Add this line
     }
 
     public function index() {
         $kategori_id = $this->input->get('kategori');
-        $data['categories'] = $this->Product_model->get_categories();
-        $data['produk_populer'] = $this->Product_model->get_popular_products_by_category($kategori_id);
+        $data['categories'] = $this->product_model->get_categories();
+        $data['produk_populer'] = $this->product_model->get_popular_products_by_category($kategori_id);
         $data['selected_category'] = $kategori_id;
         $this->load->view('popular/index', $data);
     }
