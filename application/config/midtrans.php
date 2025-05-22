f438ea4bc32ec95309211694637b5a7b2eeaaff5
Rifaiiii04 <muhamadrifay989@gmail.com>
2025-05-23 05:41:47 +0700

    add

---
+++ application/config/midtrans.php
@@ -0,0 +1,9 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+// Midtrans configuration
+$config['midtrans_server_key'] = 'YOUR_SERVER_KEY';
+$config['midtrans_client_key'] = 'YOUR_CLIENT_KEY';
+$config['midtrans_is_production'] = false;
+$config['midtrans_is_sanitized'] = true;
+$config['midtrans_is_3ds'] = true;
